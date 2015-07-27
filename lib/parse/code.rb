212ca78c11a173c28b07130e113e43f0e6b152a3
Bryan T. Meyers <btmeme@rit.edu>
2015-07-27 08:21:24 -0400

    ADDED: support for inline code

--- lib/parse/code.rb
+++ lib/parse/code.rb
@@ -1,9 +1,11 @@
 CODE = /```(\S*)\n([^`]*)```/
+INLINE_CODE = /`([^`\n]+)`/
 
 module MarkThisDown
   module Parse
     def code( wiki )
-      wiki.gsub!(CODE , '<pre code="\1">\2</pre>')
+      wiki.gsub!(CODE , '<pre> <code class="\1">\2</code></pre>')
+      wiki.gsub!(INLINE_CODE, '<code>\1</code>')
       wiki
     end
   end
